f76d3cf10205526acb44fc82297e019436187cb9
Melisa Atıcı <102025949+aticimelisa@users.noreply.github.com>
2025-05-29 20:49:10 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+  const nav = document.querySelector('.nav-links');
+  nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
+});
