84a31f5e578d602e2e65a2a595f34ea3a0a2cbc7
Melisa Atıcı <102025949+aticimelisa@users.noreply.github.com>
2025-05-31 02:13:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-// script.js
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-  const nav = document.querySelector('.nav-links');
-  nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
+// Hamburger ve menüyü seç
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav-links');
+
+
+// Hamburger tıklandığında çalışacak olan fonksiyon
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('open');        // hamburger animasyonu
+  nav.classList.toggle('show');           // menü görünür/gizli
 });
+
+
+// "Get Started" butonuna tıklandığında içerik kısmına yumuşak kaydırma
+document.querySelector('#scrollBtn').addEventListener('click', () => {
+  const target = document.querySelector('#content');
+  const offset = 460;
+
+  const targetPosition = target.getBoundingClientRect().top + window.pageYOffset - offset;
+
+  window.scrollTo({
+    top: targetPosition,
+    behavior: 'smooth'
+  });
+});
+
